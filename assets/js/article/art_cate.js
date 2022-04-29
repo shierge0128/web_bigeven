83f50f7be4a633b0837c1e5ff8076c5c7d77619b
shi2ge <2271810987@qq.com>
2022-04-29 20:13:55 +0800

    完成了article分支

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -1,108 +1,109 @@
-$(function() {
-  var layer = layui.layer
-  var form = layui.form
+$(function () {
+    var layer = layui.layer
+    var form = layui.form
+    initArtCateList()
+    // 定义获取文章分类列表函数
+    function initArtCateList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                // console.log(res);
+                var str = template('tpl-table', res)
+                $('tbody').html(str)
+            }
+        })
+    }
 
-  initArtCateList()
-
-  // 获取文章分类的列表
-  function initArtCateList() {
-    $.ajax({
-      method: 'GET',
-      url: '/my/article/cates',
-      success: function(res) {
-        var htmlStr = template('tpl-table', res)
-        $('tbody').html(htmlStr)
-      }
+    var indexAdd = null
+    // 添加文章分类按钮弹出层
+    $('#btnAddCate').on('click', function () {
+        indexAdd = layer.open({
+            type: 1,
+            area: ["500px", "250px"],
+            title: '添加文章分类',
+            content: $('#dialog-add').html()
+        })
     })
-  }
-
-  // 为添加类别按钮绑定点击事件
-  var indexAdd = null
-  $('#btnAddCate').on('click', function() {
-    indexAdd = layer.open({
-      type: 1,
-      area: ['500px', '250px'],
-      title: '添加文章分类',
-      content: $('#dialog-add').html()
-    })
-  })
-
-  // 通过代理的形式，为 form-add 表单绑定 submit 事件
-  $('body').on('submit', '#form-add', function(e) {
-    e.preventDefault()
-    $.ajax({
-      method: 'POST',
-      url: '/my/article/addcates',
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('新增分类失败！')
-        }
-        initArtCateList()
-        layer.msg('新增分类成功！')
-        // 根据索引，关闭对应的弹出层
-        layer.close(indexAdd)
-      }
-    })
-  })
-
-  // 通过代理的形式，为 btn-edit 按钮绑定点击事件
-  var indexEdit = null
-  $('tbody').on('click', '.btn-edit', function() {
-    // 弹出一个修改文章分类信息的层
-    indexEdit = layer.open({
-      type: 1,
-      area: ['500px', '250px'],
-      title: '修改文章分类',
-      content: $('#dialog-edit').html()
+    // 添加文章分类按钮功能
+    $('body').on('submit', '#form-add', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('新增文章分类失败！')
+                }
+                initArtCateList()
+                layer.msg('新增文章分类成功！')
+                layer.close(indexAdd)
+            }
+        })
     })
 
-    var id = $(this).attr('data-id')
-    // 发起请求获取对应分类的数据
-    $.ajax({
-      method: 'GET',
-      url: '/my/article/cates/' + id,
-      success: function(res) {
-        form.val('form-edit', res.data)
-      }
-    })
-  })
+    // 编辑按钮弹出层
+    var indexEdit = null
+    $('tbody').on('click', '.btn-edit', function () {
+        indexEdit = layer.open({
+            type: 1,
+            area: ["500px", "250px"],
+            title: '修改文章分类',
+            content: $('#dialog-edit').html()
+        })
+        var id = $(this).attr('data-id')
+        // console.log(id);
+        // 调用根据 Id 获取文章分类数据接口
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates/' + id,
+            success: function (res) {
+                form.val('form-edit', res.data)
 
-  // 通过代理的形式，为修改分类的表单绑定 submit 事件
-  $('body').on('submit', '#form-edit', function(e) {
-    e.preventDefault()
-    $.ajax({
-      method: 'POST',
-      url: '/my/article/updatecate',
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('更新分类数据失败！')
-        }
-        layer.msg('更新分类数据成功！')
-        layer.close(indexEdit)
-        initArtCateList()
-      }
+            }
+        })
     })
-  })
+    // 编辑按钮功能
+    $('body').on('submit', '#form-edit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/updatecate',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新分类信息失败！')
+                }
+                initArtCateList()
+                layer.msg('更新分类信息成功！')
+                layer.close(indexEdit)
 
-  // 通过代理的形式，为删除按钮绑定点击事件
-  $('tbody').on('click', '.btn-delete', function() {
-    var id = $(this).attr('data-id')
-    // 提示用户是否要删除
-    layer.confirm('确认删除?', { icon: 3, title: '提示' }, function(index) {
-      $.ajax({
-        method: 'GET',
-        url: '/my/article/deletecate/' + id,
-        success: function(res) {
-          if (res.status !== 0) {
-            return layer.msg('删除分类失败！')
-          }
-          layer.msg('删除分类成功！')
-          layer.close(index)
-          initArtCateList()
-        }
-      })
+            }
+        })
     })
-  })
+    // 删除按钮弹出层
+    $('tbody').on('click', '.btn-del', function () {
+        var id = $(this).attr('data-id')
+        layer.confirm('确定删除?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            // console.log('ok');
+
+            $.ajax({
+                method: 'GET',
+                url: '/my/article/deletecate/' + id,
+                success: function (res) {
+                    // console.log(res);
+                    if (res.status !== 0) {
+                        return layer.msg('删除文章分类失败！')
+                    }
+
+                    layer.msg('删除文章分类成功！')
+                    layer.close(indexEdit)
+                    initArtCateList()
+                }
+            })
+            layer.close(index);
+        });
+    })
 })
